d7d6e6df13a7b8a0dc2d625d8e1cda45f2cc7c69
thkim <cplkimth@naver.com>
2018-07-17 20:21:31 +0900

    .

--- UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
+++ UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
@@ -1,10 +1,113 @@
 ﻿using System;
+using System.Linq;
 
 namespace UltimateBaseball
 {
     class Program
     {
+        private static string GetInputMessage(int index)
+        {
+            string text;
+            switch (index)
+            {
+                case 0:
+                    text = "첫";
+                    break;
+                case 1:
+                    text = "두";
+                    break;
+                case 2:
+                    text = "세";
+                    break;
+                default:
+                    throw new NotImplementedException("Program.GetInputMessage");
+            }
+
+            return $"> {text} 번째 숫자를 입력하세요.";
+        }
+
         static void Main(string[] args)
+        {
+            PrintWelcomeMessage();
+
+            var numbers = GenerateNumbers();
+
+            while (true)
+            {
+                int[] guesses = InputGuesses();
+
+                PrintGuesses(guesses);
+
+                if (IsInValidGuesses(guesses))
+                {
+                    Console.WriteLine("같은 숫자를 입력하면 안 됩니다.");
+                    continue;
+                }
+
+                Result result = CalculateResult(numbers, guesses);
+
+                string resultText = FormatResult(result);
+                Console.WriteLine(resultText);
+
+                if (result.Strike == 3)
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
+            }
+        }
+
+        private static string FormatResult(Result result)
+        {
+            return $"스트라이크 {result.Strike}\n볼 {result.Ball}\n아웃 {result.Out}";
+        }
+
+        private static Result CalculateResult(int[] numbers, int[] guesses)
+        {
+            Result result = new Result();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (guesses[i] == numbers[j])
+                    {
+                        if (i == j)
+                        {
+                            result.Strike++;
+                        }
+                        else
+                        {
+                            result.Ball++;
+                        }
+                    }
+                }
+            }
+
+            result.Out = numbers.Length - result.Strike - result.Ball;
+
+            return result;
+        }
+
+        private static bool IsInValidGuesses(int[] guesses)
+        {
+            //            int count = guesses.Length;
+            //            return count != guesses.Distinct().Count();
+
+            return guesses[0] == guesses[1] || guesses[0] == guesses[2] || guesses[1] == guesses[2];
+        }
+
+        private static void PrintGuesses(int[] guesses)
+        {
+            Console.WriteLine("> 공격수가 고른 숫자");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(guesses[i]);
+            }
+        }
+
+        private static void PrintWelcomeMessage()
         {
             Console.WriteLine("+-----------------------------------------------------+");
             Console.WriteLine("|                궁극의 숫자 야구 게임                |");
@@ -16,89 +119,48 @@ namespace UltimateBaseball
             Console.WriteLine("| 숫자만 맞고 순서가 틀리면 볼입니다.                 |");
             Console.WriteLine("| 숫자가 틀리면 아웃입니다.                           |");
             Console.WriteLine("+-----------------------------------------------------+");
+        }
 
+        private static int[] InputGuesses()
+        {
+            int[] guesses = new int[3];
+
+            for (int i = 0; i < guesses.Length; i++)
+            {
+                Console.WriteLine(GetInputMessage(i));
+                guesses[i] = int.Parse(Console.ReadLine());
+            }
+
+            return guesses;
+        }
+
+        private static int[] GenerateNumbers()
+        {
             Random random = new Random();
 
             int[] numbers = new int[3];
+
             int index = 0;
             while (index < 3)
             {
                 numbers[index] = random.Next(0, 10);
 
-                bool hasDuplicate = false;
-                for (int j = 0; j < index; j++)
-                {
-                    if (numbers[index] == numbers[j])
-                    {
-                        hasDuplicate = true;
-                        break;
-                    }
-                }
-
-                if (!hasDuplicate)
-                {
-                    index++;
-                }
-            }
-
-            int[] guesses = new int[3];
-            string[] inputMessages = { "> 첫 번째 숫자를 입력하세요.", "> 두 번째 숫자를 입력하세요.", "> 세 번째 숫자를 입력하세요." };
-
-            while (true)
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.WriteLine(inputMessages[i]);
-                    guesses[i] = int.Parse(Console.ReadLine());
-                }
-
-                Console.WriteLine("> 공격수가 고른 숫자");
-
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.WriteLine(guesses[i]);
-                }
-
-                if (guesses[0] == guesses[1] || guesses[0] == guesses[2] || guesses[1] == guesses[2])
-                {
-                    Console.WriteLine("같은 숫자를 입력하면 안 됩니다.");
+                if (IsInArray(numbers[index], numbers))
                     continue;
-                }
 
-                int strikeCount = 0;
-                int ballCount = 0;
-
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        if (guesses[i] == numbers[j])
-                        {
-                            if (i == j)
-                            {
-                                strikeCount++;
-                            }
-                            else
-                            {
-                                ballCount++;
-                            }
-                        }
-                    }
-                }
-
-                Console.Write("스트라이크 ");
-                Console.WriteLine(strikeCount);
-                Console.Write("볼 ");
-                Console.WriteLine(ballCount);
-                Console.Write("아웃 ");
-                Console.WriteLine(3 - strikeCount - ballCount);
-
-                if (strikeCount == 3)
-                {
-                    Console.WriteLine("정답입니다!");
-                    break;
-                }
+                index++;
             }
+
+            return numbers;
+        }
+
+        private static bool IsInArray(int number, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == number)
+                    return true;
+
+            return false;
         }
     }
 }
